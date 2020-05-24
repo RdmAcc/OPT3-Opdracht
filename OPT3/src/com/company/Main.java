592c0f416182af09671f4b9dfe5cab1287b9eba6
Henk <flippertata12@gmail.com>
2020-05-24 16:15:41 +0100

    Added TelephoneFactory

--- OPT3/src/com/company/Main.java
+++ OPT3/src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        TelephoneFactory telephoneFactory = new TelephoneFactory();
+
+        Telephone smartphone = telephoneFactory.getTelephone("smartphone");
+        Telephone desktopTelephone = telephoneFactory.getTelephone("desktoptelephone");
+
+        smartphone.call();
+        desktopTelephone.call();
     }
 }
-
-
+//Code smells die in mijn code ontbreekt en hoe ik ze heb opgelost:
+//  +large class : refactoring -> Extract Subclass
+//  +long method: Elk method 1 responsibility geven
+//  +duplicate code: refactoring ->  -Pull Up Field
+//                                   -Extract Superclass
